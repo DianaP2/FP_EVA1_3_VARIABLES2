5f8d0b9547da83b465fe398f2fac7c7e7068968a
DianaP2 <L24550326@outlook.com>
2024-08-26 22:16:59 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLE2/src/eva1_3_variable2/EVA1_3_VARIABLE2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_variable2;
+
+/**
+ *
+ * @author melan
+ */
+public class EVA1_3_VARIABLE2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VEHICULOS
+        String marca;
+        String modelo;
+        int year; //La ñ en otros lenguajes no es aceptado
+        double precio;
+        
+        //ASIGNACION
+        marca = "Nissan";
+        modelo = "GTR";
+        year = 2008;
+        precio = 1000000.1;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+    }
+    
+}
